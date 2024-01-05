69ecdb819fb9d25d1b725a13e8ba1c883bfec503
jiska <jclassen@seemoo.tu-darmstadt.de>
2024-01-05 20:56:38 +0100

    improved version of os_log

--- scripts/os_log.js
+++ scripts/os_log.js
@@ -7,117 +7,178 @@ Interceptor.attach(isEnabledFunc, {
 });
 
 /*
-    Function that prints log args.
-    Similar to `os_log_impl_flatten_and_send`, which is a highly custom implementation.
-    It's *almost* like format strings. But all arguments are in one buffer with type and
-    length information, and there's a few more options than what's available for normal
-    format strings. So we can't just pass the arguments as a format string somewhere.
+    Function that prints OS Log args.
 
-    Also, not all functions there are exported and it's super chaotic, so parsing it on
-    our own seems to be the simpler solution.
+    Apple's clang emits built-in calls to create the OS log buffer of the correct size and
+    then also fill in the OS Log buffer. Before creating the built-in calls, OS Log takes
+    a format string and a variable number of arguments. clang then emits the buffer size
+    along with the instructions to fill the stack buffer. To this end, it first parses
+    the format string and builds an internal layout struct it fills with the arguments
+    (compile time). Then, clang iterates over the layout struct items and emits instructions
+    that end up in the binary.
+
+    CGBuiltin.cpp:          Builtin::BI__builtin_os_log_format
+    OSLog.cpp:              clang::analyze_os_log::computeOSLogBufferLayout
+    PrintfFormatString.cpp: clang::analyze_format_string::ParsePrintfString
+
+    This might be done to accelerate log message parsing, and we can use the open part
+    of the implementation to name things correctly.
+
+    At runtime, the logs are parsed by `libsystem_trace.dylib`, and the most important
+    part of the parser is contained in `os_log_impl_flatten_and_send`. Many parts in the
+    code don't contain symbols and it's difficult to call into this with Frida.
+
+    Items are defined in OSLog.h as follows:
+        4 Bits kind (upper nibble)
+             0 ScalarKind
+             1 CountKind
+             2 StringKind
+             3 PointerKind
+             4 ObjCObjKind
+        4 Bits Public/Private (lower nibble), we ignore this to print them all
+             0 Undefined
+             1 IsPrivate
+             2 IsPublic
+
+    As defined in HandlePrintfSpecifier, each of these has different cases to handle:
+         * "%f", "%d"... scalar and can be 4 bytes or even 1 bytes -- everything else is 8 bytes
+         * "%s" pointer to null-terminated string
+         * "%.*s" strlen (arg), pointer to string
+         * "%.16s" strlen (non-arg), pointer to string       -- TODO I think this is currently not handled here
+         * "%.*P" len (arg), pointer to data
+         * "%.16P" len (non-arg), pointer to data
+         * "%@" pointer to objc object
 
 */
+const kind = {
+    ScalarKind: 0,
+    CountKind: 1,
+    StringKind: 2,
+    PointerKind: 3,
+    ObjCObjKind: 4
+}
+
 const NSString = ObjC.classes.NSString;
+
 function printLog(args) {
-    //let type = args[2]; 
-    let format = args[3].readCString();
-    let buffer = args[4];
-    let num_args = buffer.add(1).readU8(); 
+    // Parse the null-terminated format string
+    let StringArg = args[3].readCString();
+    let BufAddr = args[4];
+    let BufLen = parseInt(args[5]);
 
-    if (num_args == 0) {
-        return format;
+    // The Buffer starts with two bytes: a summary byte and numArgs.
+    let numArgs = BufAddr.add(1).readU8(); 
+
+    if (numArgs == 0) {
+        return StringArg;
     }
 
-
-    //console.log(format);
-    //console.log(buffer.readByteArray(0x50));
-
-    /*
-
-                0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F  0123456789ABCDEF
-        00000000  02 03 20 08
-                            31 57 64 f9 01 00 00 00 <- ptr
-                                                    00 08 <- next type
-                                                            c0 a1  .. .1Wd.........
-        00000010  e5 04 01 00 00 00 <-- ptr
-                                    00 04 02 00 00 00 00 00 00 00  ................
-    */
-
-
-    // Most of this is a normal format string but there's '%.*P' and that one has two entries.
-    // It represents a buffer of bytes to be printed, so it's a pointer and a length.
-
-    // can't use the format string as is due to stuff like "%{private}s", "%{bluetooth:OI_STATUS}u", etc.
-    format = format.replaceAll(/%({.*?})/g, '%');
-    //console.log('format string simplified: ' + format);
+    // Can't use the format string as is due to stuff like "%{private}s", "%{bluetooth:OI_STATUS}u", etc.
+    // Quick & dirty replace of types in {}...
+    StringArg = StringArg.replaceAll(/%({.*?})/g, '%');
     
-    // list from https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFStrings/formatSpecifiers.html#//apple_ref/doc/uid/TP40004265
-    // while we could call into NSString.stringWithFormat_, Frida doesn't like varargs with vartypes here
-    let format_string_parts = format.match(/%({.*?})?.*?([@dDuUxXoOfeEgGcCsSpaAFP])/g)
-    if (format_string_parts == null) {
-        return format;
+    // Mostly normal format strings that we can match now, item by item.
+    // In our case it's easier to iterate through the format string, as the raw buffer sometimes has
+    // composed values that we want to combine and then replace one format string value with it.
+    let formatStringItems = StringArg.match(/%({.*?})?.*?([@dDuUxXoOfeEgGcCsSpaAFP])/g)
+    if (formatStringItems == null) {
+        return StringArg;
     }
-    let count = 0;
-    let offset = 2;
-    //console.log(format);
-    //console.log(format_string_parts);
-    format_string_parts.forEach((variable) => {
-        //console.log(variable);
 
-        // the buffer starts with two bytes meta information (2nd is num of args)
-        // each entry has: [1b type] [1b length - 4 or 8] [entry value]
-        let type = buffer.add(offset).readU8();
-        //console.log('type: 0x' + type.toString(16));
-        let l = buffer.add(offset + 1).readU8();
-        //console.log('len: 0x' + l.toString(16));
+    // Skip first two bytes: summary and numArgs
+    let bufferOffset = 2;
+    let itemCount = 0;
 
-        // 8 byte types (typically pointers)
-        if (l == 8) {
-            let value = buffer.add(offset + 2).readPointer();
-            //console.log('value: ' + value);
-            if (variable === '%s' && value != 0 && type >> 4 == 2) { // types 0x20 and 0x22
-                format = format.replace(variable, value.readCString());
-            } else if (variable === '%@' && value != 0 && type >> 4 == 4) { // types 0x40 and 0x42
+    formatStringItems.forEach((variable) => {
+        // each item has: [1b type] [1b length] [entry value]
+        let argDescriptor = BufAddr.add(bufferOffset).readU8();
+        let argKind = argDescriptor >> 4;
+        let argSize = BufAddr.add(bufferOffset + 1).readU8();
+
+        // StringKind, ObjcObjKind, PointerKind are all very similar 8-byte types
+        if (argKind == kind.StringKind) {
+            let value = BufAddr.add(bufferOffset + 2).readPointer();
+            if (value != 0) {
+                StringArg = StringArg.replace(variable, value.readCString());
+            }
+        } else if (argKind == kind.PointerKind) {
+            let value = BufAddr.add(bufferOffset + 2).readPointer();
+            if (value != 0) {
+                StringArg = StringArg.replace(variable, value.toString(16)); 
+            }
+
+        } else if (argKind == kind.ObjCObjKind) {
+            let value = BufAddr.add(bufferOffset + 2).readPointer();
+            if (value != 0) {
                 let c = new ObjC.Object(value);
-                format = format.replace(variable, c.description());
-            } else {
-                format = format.replace(variable, value);  // print pointer for remaining types
+                StringArg = StringArg.replace(variable, c.description());
             }
-        } else if (l == 4) {
-            let value = buffer.add(offset + 2).readU32();
-            //console.log('value: ' + value);
-
-            if (/%.*P/.test(variable) && value != 0) {
-                // can be %.*P for arbitrary buffer length or e.g. %.6P for a 6-byte buffer
-
-                // %.*P is a special case: contains a 4-byte length and an 8-byte pointer.
-                // we already read the length to value and now have to read that pointer.
-                // even if there's %.6P there's duplicate length information!
-                
-                let p = buffer.add(offset + 2 + 4 + 2).readPointer();
-                if (p != 0) {
-                    format = format.replace(variable, hexdump(p, {length: value, header: false, ansi: false}));
-                }
-                offset += 8 + 2;  // adjust for the pointer that we just read 
-            } 
-            else if (/[xX]/.test(variable)) {  // print hex as hex, but ignore padding
-                format = format.replace(variable, value.toString(16));  // preserve hex strings
-            } else {
-                format = format.replace(variable, value);
-            }
-        } else if (l == 1) {
-            let value = buffer.add(offset + 2).readU8();
-            console.log('value: ' + value);
-            format = format.replace(variable, value);
-
-        }else {
-            console.warn('!!!!!!!!!!!! unknown length!!!!!!! ' + l);
         }
-        offset += l + 2;
-        count++;
+        // CountKind describes size of next element, which is StringKind or PointerKind.
+        // CountKind is 4 bytes long.
+        // StringKind and PointerKind then point to a String or Pointer and at that position we want
+        // to read as many bytes as the count indicated.
+        else if (argKind == kind.CountKind) {
+            let value = BufAddr.add(bufferOffset + 2).readU32();
+            let pointsToKind = BufAddr.add(bufferOffset + 2 + 4).readU8() >> 4;
+            let pointerValue = BufAddr.add(bufferOffset + 2 + 4 + 2).readPointer();
+
+            if (pointerValue != 0) {
+                if (pointsToKind == kind.PointerKind) {
+                    StringArg = StringArg.replace(variable, hexdump(pointerValue, {length: value, header: false, ansi: false}));
+                } else if (pointsToKind == kind.StringKind) {
+                    StringArg = StringArg.replace(variable, pointerValue.readCString(value));
+                } else {
+                    console.error('LOG ERROR CounterKind with unknown pointer type ' + pointsToKind);
+                }
+            } else {
+                StringArg = StringArg.replace(variable, 'null');
+            }
+
+            // advance buffer by PointerKind/StringKind
+            bufferOffset += 8 + 2; 
+        }
+        // ScalarKind are regular numbers but can have different sizes
+        else if (argKind == kind.ScalarKind) {
+            let value;
+
+            // read as many bytes as the ScalarKind has
+            if (argSize == 1) {
+                value = BufAddr.add(bufferOffset + 2).readU8();
+            } else if (argSize == 4) {
+                value = BufAddr.add(bufferOffset + 2).readU32();
+            } else if (argSize == 8) {
+                value = BufAddr.add(bufferOffset + 2).readPointer();
+            }
+
+            // differentiate between hex vs decimal output
+            if (/[xX]/.test(variable)) {
+                StringArg = StringArg.replace(variable, value.toString(16));  // preserve hex strings
+            }
+            // '%0.*d' consists of two ScalarKinds where the first is number of digits
+            else if(variable === '%0.*d') {
+                let realValue = BufAddr.add(bufferOffset + 2 + 4 + 2).readU32();
+                StringArg = StringArg.replace(variable, realValue);
+                bufferOffset += 4 + 2;  // advance buffer
+            }
+            else {
+                StringArg = StringArg.replace(variable, value);
+            }
+        } else {
+            console.warn(`LOG ERROR Item parser: Unhandled object length ${argSize}!`);
+        }
+        bufferOffset += argSize + 2;    // advance offset by item description + length
+        itemCount++;
     });
 
-    return format;
+    if (bufferOffset != BufLen) {
+        console.error(`LOG ERROR buffer offset ${bufferOffset} does not match provided buffer length ${BufLen}!`);
+        console.error(BufAddr.readByteArray(BufLen));
+        console.error(formatStringItems);
+        console.error(args[3].readCString());
+    }
+
+    return StringArg;
 }
 
 // Hook all log types and print them in different colors
@@ -140,7 +201,7 @@ Interceptor.attach(log_fault, {
 
 Interceptor.attach(log_debug, {
     onEnter: function (args) {
-        console.log("\x1b[34m" + printLog(args) + "\x1b[0m");  // print debug in blue
+        console.log(`\x1b[34m${printLog(args)}\x1b[0m`);  // print debug in blue
     },
 });
 
@@ -149,14 +210,5 @@ Interceptor.attach(log_error, {
         console.error(printLog(args));
     },
 });
-
-// don't crash...
-Process.setExceptionHandler(function(exp) {
-    console.warn(JSON.stringify(Object.assign(exp, { _lr: DebugSymbol.fromAddress(exp.context.lr), _pc: DebugSymbol.fromAddress(exp.context.pc) }), null, 2));
-    Memory.protect(exp.memory.address, Process.pointerSize, 'rw-');
-    return true; // goto PC 
-});
-
-
 
 console.log('PRINT ALL THE LOGS! \\o/');
