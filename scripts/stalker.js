7acfb7a2f1d02482806f120075343284d50a3acf
jiska <jclassen@seemoo.tu-darmstadt.de>
2024-11-28 14:56:47 +0200

    basic stalker

---
+++ scripts/stalker.js
@@ -0,0 +1,30 @@
+console.log("loading");
+
+const f = Module.getExportByName(null, 'open');
+
+Interceptor.attach(f, {
+    onEnter: function(args) {
+        console.log('entered function');
+        Stalker.follow({ 
+            events: {
+                // only collect coverage for newly encountered blocks
+                compile: true,
+		//call: true,
+            },
+            onReceive: function (events) {
+                const bbs = Stalker.parse(events, {
+                    stringify: false,
+                    annotate: false
+                });
+                console.log("Stalker trace: \n" + bbs.flat().map(DebugSymbol.fromAddress).join('\n'));
+            }
+        });
+    },
+    onLeave: function(retval) {
+        Stalker.unfollow();
+        Stalker.flush();  // this is important to get all events  
+    }
+});
+
+
+console.log("loaded");
